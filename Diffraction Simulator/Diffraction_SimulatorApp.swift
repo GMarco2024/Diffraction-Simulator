fd670fe8542af13130222862a0d5b95fa4e74c17
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2025-02-07 13:57:14 -0600

    Initial Commit

---
+++ Diffraction Simulator/Diffraction_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diffraction_SimulatorApp.swift
+//  Diffraction Simulator
+//
+//  Created by Marco Gonzalez on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Diffraction_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
